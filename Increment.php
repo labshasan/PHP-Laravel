85005b1db8669e33ea1af206fef3fd28d87fce94
labshasan <labshasan@gmail.com>
2014-11-16 10:26:42 +0600

    2 new files added.

---
+++ Increment.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$a=5;
+$b=$a++;
+$c=++$a;
+echo $a;
+echo "<br/>";
+echo $b;
+echo "<br/>";
+echo $c;
+?>
+
+</body>
+</html>
---
+++ Operator1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$num1=12;
+$num2=3;
+$num3=5;
+
+echo "num1 = " . $num1;
+echo "<br/>";
+echo "num2 = " . $num2;
+echo "<br/>";
+echo "num3 = " . $num3;
+echo "<br/>";
+
+echo "<br/>";
+echo "Operations:";
+echo "<br/>";
+//
+echo "1. num1 / num2 * num3 = " . $num1 / $num2 * $num3;
+echo "<br/>";
+echo "2. num1 * num2 * num3 = " . $num1 * $num2 * $num3;
+echo "<br/>";
+echo "3. num1 + num2 * num3 = " . ($num1 + $num2 * $num3);
+echo "<br/>";
+echo "4. num1 - num2 * num3 = " . ($num1 - $num2 * $num3);
+echo "<br/>";
+echo "5. num1 % num2 * num3 = " . $num1 % $num2 * $num3;
+echo "<br/>";
+echo "6. num1 % num3 * num3 = " . $num1 % $num3 * $num3;
+echo "<br/>";
+?>
+
+</body>
+</html>
